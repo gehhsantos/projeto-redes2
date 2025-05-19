1b350f0259dd8321581fa34c37ab3ba90ebd1ec1
Geovana Santos <geovanasousafs2@hotmail.com>
2025-05-19 16:31:02 -0300

    Projeto inicial funcionando com Docker Compose e Python

---
+++ host/host.py
@@ -0,0 +1,8 @@
+import time
+import sys
+
+print("Host iniciado!", flush=True)
+
+while True:
+    print("Host está vivo...", flush=True)
+    time.sleep(5)
---
+++ roteador/roteador.py
@@ -0,0 +1,9 @@
+import time
+import sys
+
+print("Roteador iniciado!", flush=True)
+
+while True:
+    print("Roteador está vivo...", flush=True)
+    time.sleep(5)
+
