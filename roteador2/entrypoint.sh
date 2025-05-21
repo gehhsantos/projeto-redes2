8b97217ae39696042a06f958b7947ffb674591a7
Geovana Santos <geovanasousafs2@hotmail.com>
2025-05-21 18:23:51 -0300

    Atualização do projeto

--- roteador2/entrypoint.sh
+++ roteador2/entrypoint.sh
@@ -1,5 +1,13 @@
-
 #!/bin/bash
 echo "[ENTRYPONT] Script entrypoint.sh foi executado!"
+
+# Ativa o roteamento IP no sistema (essencial para funcionar como roteador)
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Adiciona regra de NAT para permitir tráfego entre redes conectadas ao roteador
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+
+# Inicia o script principal do roteador
 python3 roteador.py
+
 echo "[ENTRYPONT] roteador.py terminou."
